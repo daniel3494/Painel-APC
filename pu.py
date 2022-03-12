df1d633bf35c8bc4646ff82ddc73526d4ee11c40
igo767 <igorjustino22@gmail.com>
2022-03-12 16:49:47 -0300

    teste9

---
+++ pu.py
@@ -0,0 +1,33 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv("Pandemia\owid-covid-latest.csv")
+
+fig = px.bar(df, x="Coluna1", y="Continen", color="iso_code", barmode="group")
+opcoes = list(df["Coluna5"].unique())
+opcoes.append("Todas as lojas")
+    
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
