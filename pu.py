7d179bd0ebbaa652ba81fee6bd65a5d97ddff108
DanielRogs <danielrocha.dr142@gmail.com>
2022-03-12 20:24:35 -0300

    Atualização no gráfico

--- pu.py
+++ pu.py
@@ -7,12 +7,25 @@ import pandas as pd
 
 app = Dash(__name__)
 
+colors = {
+    'background': '#282120',
+    'text': '#F8EFE4'
+}
+
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
+
 df = pd.read_excel("Painel-APC\Excelchips.xlsx")
-fig = px.line(df, x='Datas', y='Preço', color='Categoria', markers=True)
+
+fig = px.line(df, x='Datas', y='Preço', color='Categoria', markers=True, height=580, text="Preço")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
     
-app.layout = html.Div(children=[
+app.layout = html.Div(style={'backgroundColor': "#282120"} ,children=[
     html.H1(children='Preços de Produtos dependentes de microchips', style={
         "textAlign": "center",
         "font-family": "Century Gothic",
@@ -29,9 +42,7 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure=fig
     )
-], style={
-    "background-color": "#274472"
-})
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
