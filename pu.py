66b40467594ad1fe3c45970057b52d7a0907a675
DanielRogs <danielrocha.dr142@gmail.com>
2022-03-12 18:30:21 -0300

    Remodelagem do gráfico

--- pu.py
+++ pu.py
@@ -9,22 +9,29 @@ app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.read_excel("https://github.com/Trabalho-APC-DASH/Painel-APC/blob/main/Excelchips.xlsx?raw=true")
-
-fig = px.bar(df, x="Categoria", y="12_03_2020")
+df = pd.read_excel("Painel-APC\Excelchips.xlsx")
+fig = px.line(df, x='Datas', y='Preço', color='Categoria', markers=True)
     
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Preços de Produtos dependentes de microchips', style={
+        "textAlign": "center",
+        "font-family": "Century Gothic",
+        "color": "white"
+    }),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+    html.Div(children='Gráfico baseado em peços no ano de 2020', style={
+        "textAlign": "center",
+        "font-family": "Century Gothic",
+        "color": "white"
+    }),
 
     dcc.Graph(
         id='example-graph',
         figure=fig
     )
-])
+], style={
+    "background-color": "#274472"
+})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
