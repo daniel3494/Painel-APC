46a70888a832d3e0306f215603a10fd2a2adb23d
DanielRogs <danielrocha.dr142@gmail.com>
2022-03-14 18:37:30 -0300

    Atualização - 14/03 - 18:37

--- Gráfico_Pizza_LucroAnualProdutos.py
+++ Gráfico_Pizza_LucroAnualProdutos.py
@@ -1,10 +1,9 @@
 import plotly.express as px
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output
-from plotly.subplots import make_subplots
 
 df = pd.read_excel("Painel-APC\Agricultura-valor-da-produo-brasil.xlsx")
 
-fig = px.
+fig = px.pie(df, values="valor", names="produto",title="Lucro Anual dos Principais Produtos Agrícolas", hole= .3, color_discrete_sequence=px.colors.sequential.RdBu)
 
 fig.show()
