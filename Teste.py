4108d38195d24f84d20431ff50c63c5e53412b54
DanielRogs <danielrocha.dr142@gmail.com>
2022-03-14 18:16:24 -0300

    Commit

--- Teste.py
+++ Teste.py
@@ -4,17 +4,7 @@ from dash import Dash, dcc, html, Input, Output
 from plotly.subplots import make_subplots
 
 df = pd.read_excel("Painel-APC\Agricultura-valor-da-produo-brasil.xlsx")
-df2 = pd.read_excel("Painel-APC\Brasil-Exportacao_cafe_por_pais.xlsx")
 
-fig = make_subplots(
-    rows=2, cols=1,
-    row_heights=[1.6, 1.6],
-    specs=[
-        [{"type": "bar"}],
-        [{"type": "pie"}]
-    ]
-)
-
-
+fig = px.
 
 fig.show()
