75596125a3849c7d741e14687b7ab7d9c73594d4
DanielRogs <danielrocha.dr142@gmail.com>
2022-03-14 18:38:54 -0300

    Link com o GitHub - 14/03 - 18:38

--- Gráfico_Pizza_LucroAnualProdutos.py
+++ Gráfico_Pizza_LucroAnualProdutos.py
@@ -2,7 +2,7 @@ import plotly.express as px
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 
-df = pd.read_excel("Painel-APC\Agricultura-valor-da-produo-brasil.xlsx")
+df = pd.read_excel("https://github.com/Trabalho-APC-DASH/Painel-APC/blob/main/Agricultura-valor-da-produo-brasil.xlsx?raw=true")
 
 fig = px.pie(df, values="valor", names="produto",title="Lucro Anual dos Principais Produtos Agrícolas", hole= .3, color_discrete_sequence=px.colors.sequential.RdBu)
 
