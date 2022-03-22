472eee10548756e2dd3f99742372832bb3bdc81a
DanielRogs <danielrocha.dr142@gmail.com>
2022-03-22 12:02:27 -0300

    Nova Versão de Pizza - 22/03 - 12:02

--- Gráfico_Pizza_LucroAnualProdutos.py
+++ Gráfico_Pizza_LucroAnualProdutos.py
@@ -1,9 +1,11 @@
 import plotly.express as px
-import pandas as pd
-from dash import Dash, dcc, html, Input, Output
+import plotly.graph_objects as go
 
-df = pd.read_excel("https://github.com/Trabalho-APC-DASH/Painel-APC/blob/main/Banco%20de%20Dados/Agricultura-valor-da-produo-brasil.xlsx?raw=true")
-
-fig = px.pie(df, values="valor", names="produto",title="Lucro Anual dos Principais Produtos Agrícolas", hole= .3, color_discrete_sequence=px.colors.sequential.RdBu)
+fig =go.Figure(go.Sunburst(
+    labels=["Lucros", "Soja", "Milho em Grãos", "Cana-de-Açúcar", "Café", "Algodão herbáceo", "Arroz", "Laranja", "Mais de R$ 50.000.000", "Menos de R$ 50.000.000"],
+    parents=["", "Mais de R$ 50.000.000", "Mais de R$ 50.000.000", "Mais de R$ 50.000.000", "Menos de R$ 50.000.000", "Menos de R$ 50.000.000", "Menos de R$ 50.000.000", "Menos de R$ 50.000.000", "Lucros", "Lucros"],
+    values=[10, 169100228, 73949252, 60800886 , 27254184, 19127892, 11631701, 10898251, 169100228 + 73949252 + 60800886, 27254184 + 19127892 + 11631701 + 10898251],
+))
+fig.update_layout(margin = dict(t=50, l=0, r=0, b=0))
 
 fig.show()
