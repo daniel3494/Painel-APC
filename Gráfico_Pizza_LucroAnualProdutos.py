c9d55c82e3bf053177c19cc4763be007cbf543bf
DanielRogs <danielrocha.dr142@gmail.com>
2022-03-22 17:47:20 -0300

    Nova Versão V2.0 - 22/03 - 17:47

--- Gráfico_Pizza_LucroAnualProdutos.py
+++ Gráfico_Pizza_LucroAnualProdutos.py
@@ -4,7 +4,7 @@ import plotly.graph_objects as go
 fig =go.Figure(go.Sunburst(
     labels=["Lucros", "Soja", "Milho em Grãos", "Cana-de-Açúcar", "Café", "Algodão herbáceo", "Arroz", "Laranja", "Mais de R$ 50.000.000", "Menos de R$ 50.000.000"],
     parents=["", "Mais de R$ 50.000.000", "Mais de R$ 50.000.000", "Mais de R$ 50.000.000", "Menos de R$ 50.000.000", "Menos de R$ 50.000.000", "Menos de R$ 50.000.000", "Menos de R$ 50.000.000", "Lucros", "Lucros"],
-    values=[10, 169100228, 73949252, 60800886 , 27254184, 19127892, 11631701, 10898251, 169100228 + 73949252 + 60800886, 27254184 + 19127892 + 11631701 + 10898251],
+    values=[0, 169100228, 73949252, 60800886 , 27254184, 19127892, 11631701, 10898251, 169100228 + 73949252 + 60800886, 27254184 + 19127892 + 11631701 + 10898251],
 ))
 fig.update_layout(margin = dict(t=50, l=0, r=0, b=0))
 
